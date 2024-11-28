9d039beaebe50ec16b1cc367e3d7f8c1f5be14a2
Subhojit <subhojitchowhan4@gmail.com>
2024-11-28 18:24:06 +0530

    Update hero image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image"
 import Patterns from "./Patterns"
-import MyImage from '@/app/assets/Images&Icons/IMG_20241128_174707.webp'
+import MyImage from '@/app/assets/Images&Icons/IMG_20241128_174707.webp
 import { useEffect, useRef } from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
 import Lenis from '@studio-freight/lenis'
